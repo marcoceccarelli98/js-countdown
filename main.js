f3bc6daa09fc234124f594b05050854ddbeba16b
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-13 15:56:59 +0200

    [Bonus 2] Added stop button

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const cdOutput = document.getElementById('countdown');
+const btnStop = document.querySelector('.btn');
+
 
 let c = 10;
 
 const myCountdown = setInterval(countdown, 1000);
+
+btnStop.addEventListener('click', function () {
+    c = -1;
+});
 
 function countdown() {
     if (c >= 0) {
